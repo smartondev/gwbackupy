ed296c299a385b9ca1514252c6050fecce27502f
Márton Somogyi <kamarton@gmail.com>
2022-12-20 04:41:25 +0100

    setup.py add long description

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 
 # CHANGELOG
 
+## 0.2.0 - 2012-12-20
+
+`setup.py` fixes and additions
+
 ## 0.1.0 - 2022-12-20
 
 Initial public release
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='gwbackupy',
-    version='0.1.1',
+    version='0.2.0',
     packages=['gwb'],
     url='https://github.com/smartondev/gwbackupy',
     license='BSD 3-Clause "New" or "Revised" License',
     author='Márton Somogyi',
     author_email='info@smarton.dev',
     description='Open source Google Workspace backup solution.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['google-api-python-client~=2.70.0', 'oauth2client~=4.1.3', 'pyopenssl~=22.1'],
     scripts=['scripts/gwbackupy'],
     classifiers=[
