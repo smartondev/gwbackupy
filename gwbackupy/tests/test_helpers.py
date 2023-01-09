07e7ad3b8432f686a955ded13281bcf5e274a184
Márton Somogyi <kamarton@gmail.com>
2023-01-09 13:36:46 +0100

    #28 add tests for helpers

--- gwbackupy/tests/test_helpers.py
+++ gwbackupy/tests/test_helpers.py
@@ -1,4 +1,6 @@
-from gwbackupy.helpers import str_trim, decode_base64url
+import io
+
+from gwbackupy.helpers import str_trim, decode_base64url, encode_base64url, json_load
 
 
 def test_str_trim():
@@ -11,3 +13,13 @@ def test_str_trim():
 def test_decode_base64url():
     assert decode_base64url("YQ") == b"a"
     assert decode_base64url("YjY0") == b"b64"
+
+
+def test_encode_base64url():
+    assert encode_base64url(b"a") == "YQ"
+    assert encode_base64url(b"b64") == "YjY0"
+
+
+def test_json_load():
+    assert json_load(io.BytesIO(b'{"a": 1}')) == {"a": 1}
+    assert json_load(io.BytesIO(b'{"a": 1, "b":}')) is None
