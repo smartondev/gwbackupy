522412b19b2daac6b708af85b54d927af7b94e91
Márton Somogyi <kamarton@gmail.com>
2023-01-12 03:46:25 +0100

    removed imports

--- gwbackupy/tests/test_gmail.py
+++ gwbackupy/tests/test_gmail.py
@@ -1,10 +1,6 @@
-import tempfile
-
 from gwbackupy.gmail import Gmail
-from gwbackupy.storage.file_storage import FileStorage
 from gwbackupy.tests.mock_storage import MockStorage
 from gwbackupy.tests.test_mock_gmail_service_wrapper import MockGmailServiceWrapper
-from gwbackupy.tests.test_mock_service_provider import MockServiceProvider
 
 
 def test_empty_server_backup():
